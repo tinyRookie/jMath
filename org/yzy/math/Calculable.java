7406f1eba43f6ac4204833262bb51200b5eef8a8
yangzhiyue <zjyzy19920513@gmail.com>
2017-08-11 00:06:44 +0800

    2017.08.09

--- org/yzy/math/Calculable.java
+++ org/yzy/math/Calculable.java
@@ -3,5 +3,7 @@ package org.yzy.math;
 /**
  * Created by yangzhiyue on 17/8/9.
  */
-public interface Calculable {
+public interface Calculable extends Serializable{
+	Calculable add(Calculable calc);
+	
 }
